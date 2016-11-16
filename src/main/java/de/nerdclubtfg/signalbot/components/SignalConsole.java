f04b2310d64ea3a97a282116e1cfe5f1e98bf740
Turakar <turakar23@gmail.com>
2016-11-16 19:51:53 +0100

    Add saving of attachments in offline mode (#18)
    Add saving of attachments in offline mode

--- src/main/java/de/nerdclubtfg/signalbot/components/SignalConsole.java
+++ src/main/java/de/nerdclubtfg/signalbot/components/SignalConsole.java
@@ -1,10 +1,15 @@
 package de.nerdclubtfg.signalbot.components;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import org.whispersystems.signalservice.api.messages.SignalServiceAttachment;
 import org.whispersystems.signalservice.api.messages.SignalServiceDataMessage;
 
 import de.thoffbauer.signal4j.listener.ConversationListener;
@@ -29,6 +34,29 @@ public class SignalConsole extends Signal {
 				+ "%s",
 				String.join(", ", addresses),
 				message.getBody().or("no body"));
+		if(message.getAttachments().isPresent() && !message.getAttachments().get().isEmpty()) {
+			Path attachments = Paths.get("attachments");
+			if(!Files.exists(attachments)) {
+				Files.createDirectory(attachments);
+			}
+			message.getAttachments().get().stream()
+					.map(SignalServiceAttachment::asStream)
+					.map(stream -> {
+						Path path = attachments.resolve("bot" + System.currentTimeMillis() + "." 
+													+ stream.getContentType().split("/")[1]);
+						try {
+							Files.copy(stream.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+						} catch (IOException e) {
+							System.err.println("Could not save an attachment! " + e.getMessage() + " (" + e.getClass().getSimpleName() + ")");
+							return null;
+						}
+						return path;
+					})
+					.filter(p -> p != null)
+					.forEach(p -> {
+						System.out.println("Saved an attachment to " + p.toString());
+					});
+		}
 		System.out.println(out);
 	}
 
@@ -47,6 +75,7 @@ public class SignalConsole extends Signal {
 		while(!(line = scanner.nextLine()).isEmpty()) {
 			body += line + "\n";
 		}
+		body = body.substring(0, body.length() - 1); // strip off last \n
 		
 		SignalServiceDataMessage message = SignalServiceDataMessage.newBuilder()
 				.withTimestamp(System.currentTimeMillis())
